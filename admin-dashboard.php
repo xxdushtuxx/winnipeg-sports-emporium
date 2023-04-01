758a2ffc16c6263ae965a765eb3fc4b864203f73
xxdushtuxx <96920561+xxdushtuxx@users.noreply.github.com>
2023-04-01 18:18:25 -0500

    Adding admin dashboard page

---
+++ admin-dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="addProducts.php">Add a new product</a>
+    <br>
+    <a href="categories.php">Add a new category</a>
+    <br>
+    <a href="edit.php">Edit a product</a>
+</body>
+</html>
