9323fc7fd609173f8dc372e7ec874187f78e1687
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-04-16 04:10:00 -0500

    signup functionality added and updated

--- signup.php
+++ signup.php
@@ -1,4 +1,42 @@
 <?php
+require('connect.php');
+$errorFlag = false;
+
+// Check if form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get form data
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $passwordConfirm = $_POST['password-confirm'];
+
+
+
+    // Check if passwords match
+    if ($password !== $passwordConfirm) {
+        $errorFlag = true;
+        $errorMessage = 'Passwords do not match. Please try again.';
+    } else {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        
+        $query = "INSERT INTO users (user_name, user_email, user_firstname, user_lastname, user_password) VALUES (:uname, :uemail, :ufirstname , :ulastname, :upassword)";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':uname', $username);        
+        $statement->bindValue(':uemail', $email);
+        $statement->bindValue(':ufirstname', $firstName);
+        $statement->bindValue(':ulastname', $lastName);
+        $statement->bindValue(':upassword', $hashedPassword);
+        $statement->execute();
+
+        // Redirect to login page
+        header('Location: login.php');
+        exit;
+    }
+}
 
 ?>
 
@@ -75,18 +113,22 @@
         <label for="phone">Phone #:</label>
         <input type="tel" name="phone" id="phone" required>
 
-        <label for="shippingAddress">Shipping Address:</label>
-        <input type="text" name="shippingAddress" id="shippingAddress" required>
-
         <label for="username">Username:</label>
         <input type="text" name="username" id="username" required>
 
         <label for="password">Password:</label>
         <input type="password" name="password" id="password" required>
 
+        <label for="password">Password confirm:</label>
+        <input type="password" name="password-confirm" id="password-confirm" required>
+
         <input type="submit" value="Sign Up">
 
     </form>
     <div class="signup-link">Already have an account? <a href="login.php">Log in</a></div>
+
+    <?php if($errorFlag): ?>
+        <?= $errorMessage ?>
+    <?php endif ?>
 </body>
 </html>
