29fc755147db328d48990371e713e5658e268c3e
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-04-16 04:15:35 -0500

    comment functionality added for all users.

--- comment.php
+++ comment.php
@@ -1,12 +1,78 @@
-<?php
-
+<?php 
 require('connect.php');
+
 session_start();
 
 
+$message = "";
 
+if(isset($_GET['id'])){
+    $_SESSION['commentProductID'] = $_GET['id'];
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+
+    
+    if(isset($_SESSION['user_id'])){
+
+        $productID = $_SESSION['commentProductID']; 
+        $userID = $_SESSION['user_id']; 
+
+        $sql = "SELECT * FROM users WHERE user_id = :userID";
+        $istatement = $db->prepare($sql);
+        $istatement->bindValue(':userID', $userID);
+        $istatement->execute();
+
+        $info = $istatement->fetch();
+
+        $name = $info['user_firstname'] . ' ' . $info['user_lastname'];
+        $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+        
+        if(empty($comment)){
+            echo "one is empty";
+        }
+
+        // Insert comment into database
+        $query = "INSERT INTO comments (product_id, user_id, user_commentername, comment) VALUES (:productID, :userID, :uname, :comment)";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':productID', $productID, PDO::PARAM_INT);
+        $statement->bindValue(':userID', $userID, PDO::PARAM_INT);
+        $statement->bindValue(':uname', $name);
+        $statement->bindValue(':comment', $comment);
+        $statement->execute();
+
+        $_SESSION['commentName'] = "";
+        $_SESSION['commentText'] = "";
+
+    } else{
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+        $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+        
+        if(strlen(trim($name)) === 0){
+            $name = "Anonymous";
+        }
+
+        $productID = $_SESSION['commentProductID']; // replace with your product ID
+        $userID = 0; // replace with your user ID
+
+        // Insert comment into database
+        $query = "INSERT INTO comments (product_id, user_id, user_commentername, comment) VALUES (:productID, :userID, :uname, :comment)";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':productID', $productID, PDO::PARAM_INT);
+        $statement->bindValue(':userID', $userID, PDO::PARAM_INT);
+        $statement->bindValue(':uname', $name);
+        $statement->bindValue(':comment', $comment);
+        $statement->execute();
+
+        $_SESSION['commentName'] = "";
+
+
+}
+    
+}
 ?>
-
 
 <html lang="en">
 <head>
@@ -17,29 +83,20 @@ session_start();
 </head>
 <body>
 <form method="post" action="comment.php">
+
     <?php if(!isset($_SESSION['user_id'])): ?>
-
-    <label for="firstname">First Name:</label>
-    <input type="text" name="firstname" required>
-<br>
-    <label for="lastname">Last Name:</label>
-    <input type="text" name="lastname" required>
-
+    <label for="name">Name:</label>
+    <input type="text" name="name" value="<?= $_SESSION['commentName'] ?>" >
     <?php endif ?>
-<br>
+    <br>
     <label for="comment">Comment:</label>
-    <br>
-    <textarea name="comment" required></textarea>
-<br>
-    <label for="captcha">Captcha:</label>
-    <br>
-    <img src="captcha.php" alt="CAPTCHA"><br>
-    <input type="text" name="captcha" required>
-
-    <input type="hidden" name="id" value="<?= $_SESSION['user_id'] ?>">
-    <input type="hidden" name="id" value="0">
-    <input type="submit" value="Submit">
+    <input type="text" name="comment" value="<?= $_SESSION['commentText'] ?>" required>
+    
+    
 </form>
 
+<?php if($captchaFailure): ?>
+<p><?= $message ?></p>
+    <?php endif ?>
 </body>
 </html>
