ece7004b7c47e042af158042353ce41133e8d426
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-04-16 04:36:27 -0500

    admin accessible all functions are added here

---
+++ adminDashboard.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+</head>
+<body>
+    <a href="addProduct.php">Add a new product</a>
+    <br>
+    <a href="categories.php">Add a new category</a>
+    <br>
+    <a href="editProduct.php">Edit a product</a>
+    <br>
+    <a href="editCategory.php">Edit a category</a>
+    <br>
+    <a href="editUser.php">Edit User</a>
+    <br>
+    <a href="addUser.php">Add User</a>
+    <br>
+    <a href="editComment.php">Moderate Comments</a>
+</body>
+</html>
